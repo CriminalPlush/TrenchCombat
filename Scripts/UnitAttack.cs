ad8a8476cf96f40059c68f1d5abade1375424f8d
CriminalPlushToy <mgamzat420@gmail.com>
2025-03-17 21:14:26 +0300

    Added some random rocks
    ♂Rock and stone!♂

--- Scripts/UnitAttack.cs
+++ Scripts/UnitAttack.cs
@@ -101,7 +101,6 @@ public class UnitAttack : MonoBehaviour
                 {
                     if (sound.loop)
                     {
-                   Debug.Log("fuck your moder");
                         sound.Pause();
                     }
                 }
