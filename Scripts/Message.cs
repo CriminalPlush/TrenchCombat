395dfd4ce306bf63eccc0e2be33cf7ea9d93b9e8
CriminalPlushToy <mgamzat420@gmail.com>
2025-04-04 00:51:43 +0300

    04.04.2025
    Just started adding localization and dialog system.

---
+++ Scripts/Message.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message : MonoBehaviour
+{
+    public Messenger messenger;
+    public string message;
+}
---
+++ Scripts/Messenger.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Messenger : ScriptableObject
+{
+    public Texture img;
+}
